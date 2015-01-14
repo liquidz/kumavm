90e4c012d15d87a56c36cb5aaeab5bdbb36e1831
liquidz <liquidz.uo@gmail.com>
2015-01-14 23:56:14 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install -y ansible
+
+cat <<EOT > /tmp/hosts
+127.0.0.1
+EOT
+sudo /bin/mv -f /tmp/hosts /etc/ansible/hosts
+
+ansible-playbook /vagrant/playbook.yml
